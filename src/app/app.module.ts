83f1454571d9a4df6d7c55baba3137dd61aa57c1
Gabriela Flores <gabiiflores09@gmail.com>
2019-03-04 19:08:40 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewsContentComponent } from './news-content/news-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
